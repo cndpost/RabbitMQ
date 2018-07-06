5352a70f2371aefc29576a935d13af7420f6b83d
Xu <John.Xu@envestnetpmc.net>
2018-07-06 13:55:23 -0700

    RabbitMQ Tutotial initial check in

---
+++ RabbitMQTutorial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitMQTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
